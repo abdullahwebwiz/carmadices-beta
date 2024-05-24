ca09133f76000b3cc42fefaa13de59f7d1c471ff
abdullahwebwiz <abdullahwebwiz@gmail.com>
2024-05-24 15:32:45 +0500

    puzzle xx2

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,17 +2,26 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import vercel from "vite-plugin-vercel";
 import { resolve } from "path";
+// export default defineConfig({
+//   build: {
+//     manifest: true,
+//     rollupOptions: {
+//       input: 'src/main.tsx',
+//     },
+//   },
+//   // server: {
+//   //   port: 8080, // Specify the port
+//   // },
+//   plugins: [react(), vercel()],
+// });
+
 export default defineConfig({
   build: {
-    manifest: true,
     rollupOptions: {
-      input: 'src/main.tsx',
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
     },
   },
-  // server: {
-  //   port: 8080, // Specify the port
-  // },
   plugins: [react(), vercel()],
 });
-
-
